972b07711395267c8c9d6141bd19e89c5006084e
terencecistudent <terencecistudent@users.noreply.github.com>
2021-08-02 21:12:32 +0100

    Materialize CSS Example Portfolio showcase near complete

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+  $(".materialboxed").materialbox();
+  $(".collapsible").collapsible();
+  $(".fixed-action-btn").floatingActionButton();
+});
